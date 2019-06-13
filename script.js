d1927e9b37481bd681eac1f847182c66d4a370fb
Sofronijev <sofronijev@gmail.com>
2019-06-13 11:05:02 +0200

    prevod na engleski

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
 $(document).ready(function () {
 
 
-    let slike = ["./db/goku.jpg", "./db/krillin.jpg", "./db/vegeta.jpeg", "./db/gohan.jpg", "./db/roshi.jpg", "./db/chichi.jpg", "./db/yamcha.jpg", "./db/chautzu.jpg", "./db/frieza.jpg", "./db/cell.jpg", "./db/android18.jpg", "./db/kidbuu.jpg", "./db/pikolo.jpg", "./db/tien.jpeg", "./db/trunks.jpg"]
+    let pictures = ["./db/goku.jpg", "./db/krillin.jpg", "./db/vegeta.jpeg", "./db/gohan.jpg", "./db/roshi.jpg", "./db/chichi.jpg", "./db/yamcha.jpg", "./db/chautzu.jpg", "./db/frieza.jpg", "./db/cell.jpg", "./db/android18.jpg", "./db/kidbuu.jpg", "./db/pikolo.jpg", "./db/tien.jpeg", "./db/trunks.jpg"]
 
     //prvo dugme
-    $("#prikazi").click(function () {
-        let brojSlika = $("#broj").val();
+    $("#showBtn").click(function () {
+        let picturesNumber = $("#number").val();
         //proveravam da li je korisnik uneo dobar broj
-        if (brojSlika > 15 || brojSlika < 1) {
+        if (picturesNumber > 15 || picturesNumber < 1) {
             window.alert("Unesite broj slika izmedju 1 i 15!");
             return false;
         }
         //za unet broj, dodaje slike
-        for (let i = 0; i < brojSlika * 2; i++) {
-            $("#karte").append("<div class='kartica'><img class='prednjaStrana' src=''><img class='zadnjaStrana' src='./db/kugla.jpg' alt='kugla'></div>");
+        for (let i = 0; i < picturesNumber * 2; i++) {
+            $("#board").append("<div class='memoryCard'><img class='cardFront' src=''><img class='cardBack' src='./db/kugla.jpg' alt='kugla'></div>");
         }
 
         //ovde treba da dodajem razlicite slike, 'i' se povecava za 2 da bi na po 2 karte stavio istu sliku
-        for (let i = 0, y = 0; i < brojSlika * 2, y < brojSlika; i += 2, y++) {
-            $("#karte .kartica:eq(" + i + ") .prednjaStrana").attr("src", slike[y]);
-            $("#karte .kartica:eq(" + (i + 1) + ") .prednjaStrana").attr("src", slike[y]);
+        for (let i = 0, y = 0; i < picturesNumber * 2, y < picturesNumber; i += 2, y++) {
+            $("#board .memoryCard:eq(" + i + ") .cardFront").attr("src", pictures[y]);
+            $("#board .memoryCard:eq(" + (i + 1) + ") .cardFront").attr("src", pictures[y]);
         }
 
         //rotira karticu da se vidi na pocetku
-        $(".kartica").addClass(" rotiraj");
-        $(".prednjaStrana").addClass(" rotiraj");
+        $(".memoryCard").addClass(" rotate");
+        $(".cardFront").addClass(" rotate");
 
         //iskljuci se da ne moze da se vise puta klikce
-        $("#prikazi").prop('disabled', true);
+        $("#showBtn").prop('disabled', true);
         //iskljucim input, da ne moze da se menja
-        $("#broj").prop('disabled', true);
+        $("#number").prop('disabled', true);
 
     });
 
-    $("#promesaj").click(function () {
+    $("#startBtn").click(function () {
         //provera da li su izabrene slike
-        if ($("#prikazi").prop('disabled') == true) {
+        if ($("#showBtn").prop('disabled') == true) {
 
             //iskljucujem dugme da ne moze vise da se pritiska
-            $("#promesaj").prop('disabled', true);
+            $("#startBtn").prop('disabled', true);
             //uvodim ponovo promenljivu broj slika
-            let brojSlika = $("#broj").val();
+            let picturesNumber = $("#number").val();
 
             //okrecem karte
-            $(".kartica").removeClass(" rotiraj");
-            $(".prednjaStrana").removeClass(" rotiraj");
+            $(".memoryCard").removeClass(" rotate");
+            $(".cardFront").removeClass(" rotate");
 
             //pravim niz sa random brojevima
-            let randomBrojevi = []
-            while (randomBrojevi.length < brojSlika * 2) {
-                var r = Math.floor(Math.random() * brojSlika * 2) + 1;
-                if (randomBrojevi.indexOf(r) === -1) randomBrojevi.push(r);
+            let randomNumbers = []
+            while (randomNumbers.length < picturesNumber * 2) {
+                var r = Math.floor(Math.random() * picturesNumber * 2) + 1;
+                if (randomNumbers.indexOf(r) === -1) randomNumbers.push(r);
             }
             //mesam karte sa nizom random brojeva i orderom
-            function mesanje() {
-                for (let i = 0; i < brojSlika * 2; i++) {
-                    $("#karte .kartica:eq(" + i + ")").css("order", randomBrojevi[i]);
+            function suffleCards() {
+                for (let i = 0; i < picturesNumber * 2; i++) {
+                    $("#board .memoryCard:eq(" + i + ")").css("order", randomNumbers[i]);
                 }
             }
             //ovo sam postavio jer kad nema timeouta, kad se okrecu karte odmah se vidi na kratko gde se koja karta rasporedila prilikom mesanja
-            setTimeout(mesanje, 200);
+            setTimeout(suffleCards, 200);
 
             //Tajmer
-            let pauza = false;
+            let disableBoard = false;
             let timeRemaining = 60;
             let timerField = $("#timer");
             let timer = setInterval(() => {
@@ -73,15 +73,15 @@ $(document).ready(function () {
                     //ne moze vise da se sklikce na kartice                   
                     $("#endGame").text("DEFEAT!");
                     endTextOn();
-                    pauza = true;
+                    disableBoard = true;
                 }
             }, 1000);
 
-            let okrenutaKartica = false;
-            let prvaOkrenuta;
-            let drugaOkrenuta;
-            let brojPoteza = 0;
-            let brojPogodaka = 0;
+            let flippedCard = false;
+            let firstFlipped;
+            let secondFlipped;
+            let totalMoves = 0;
+            let cardsMatched = 0;
 
             //fukncija koja pokazuje tekst kad se zavrsi igra
             function endTextOn() {
@@ -93,55 +93,55 @@ $(document).ready(function () {
             }
             $("#overlay").click(endTextOff);
             //okrecem kartice na klik
-            $(".kartica").click(function () {
+            $(".memoryCard").click(function () {
                 //ako postoje vec dve otkrivene ceka se da se vrate ili ako je isteklo vreme
-                if (pauza) {
+                if (disableBoard) {
                     return false;
                 }
 
-                //dodaje klasu rotiraj na klikutu karticu
-                $(this).addClass(" rotiraj");
+                //dodaje klasu rotate na klikutu karticu
+                $(this).addClass(" rotate");
                 //da li je prvi klik
-                if (!okrenutaKartica) {
-                    okrenutaKartica = true;
-                    prvaOkrenuta = $(this);
+                if (!flippedCard) {
+                    flippedCard = true;
+                    firstFlipped = $(this);
 
                     //ako nije onda je drugi    
                 } else {
                     //ovde pazimo da se nije dva puta kliknulo na istu kartu
                     //ako je karta na koju smo kliknuli ista kao prva nakoju smo kliknuli onda false
-                    if ($(this).css("order") == prvaOkrenuta.css("order")) {
+                    if ($(this).css("order") == firstFlipped.css("order")) {
                         return false;
                     }
-                    okrenutaKartica = false;
-                    drugaOkrenuta = $(this);
+                    flippedCard = false;
+                    secondFlipped = $(this);
                     //proveravam da li se slike poklapaju kod okrenutih kartica
-                    if (prvaOkrenuta.children(".prednjaStrana").attr("src") ==
-                     drugaOkrenuta.children(".prednjaStrana").attr("src")) {
+                    if (firstFlipped.children(".cardFront").attr("src") ==
+                        secondFlipped.children(".cardFront").attr("src")) {
                         //iskljucujemo klik efekat da uparenim karticama
-                        prvaOkrenuta.off("click");
-                        drugaOkrenuta.off("click");
+                        firstFlipped.off("click");
+                        secondFlipped.off("click");
                         //registruje se pogodak, broji potez i ispisuje na stranici
-                        brojPogodaka++;
-                        brojPoteza++;
-                        $("#pogodak").text(brojPoteza);
+                        cardsMatched++;
+                        totalMoves++;
+                        $("#moves").text(totalMoves);
 
                     } else {
                         //pauzira se funkcija da se okrecu karte dok se ove dve ne vrate
-                        pauza = true;
+                        disableBoard = true;
                         //kartice se okrecu, settimeout je da bi se videle na kratko
                         setTimeout(function () {
-                            prvaOkrenuta.removeClass("rotiraj");
-                            drugaOkrenuta.removeClass("rotiraj");
-                            pauza = false;
+                            firstFlipped.removeClass("rotate");
+                            secondFlipped.removeClass("rotate");
+                            disableBoard = false;
                         }, 1000);
-                        brojPoteza++;
-                        $("#pogodak").text(brojPoteza);
+                        totalMoves++;
+                        $("#moves").text(totalMoves);
 
 
                     }
                 }
-                if (brojPogodaka == brojSlika) {
+                if (cardsMatched == picturesNumber) {
                     clearInterval(timer);
                     $("#endGame").text("VICTORY!");
                     endTextOn();
